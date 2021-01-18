691cd5b1bc868c47bbb11a6872e06e5bfa7d9cae
Trisentosa W <trisentosawisesa@gmail.com>
2021-01-18 14:40:49 -0600

    fix acces error

--- public/javascripts/citiesAndCountries.js
+++ public/javascripts/citiesAndCountries.js
@@ -1,5 +1,5 @@
 import cityDatas from '../data/citiesAndCountries.js'
-import usDatas from '../data/uscitiesAndStates.js'
+import usDatas from '../data/usCitiesAndStates.js'
 
 const states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", 'Lousiana', "Maine", "Maryland","Massachusetts","Michigan", "Minnesotta", "Mississippi", "Missouri","Montana", 'Nebraska', "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma","Oregon","Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 const stateID = ["01", "54", "02", "03", "04", "05", "06", "07", "08", "09", "10", "52", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
