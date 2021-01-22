c4e8842e58d60a4a8610596b2488e4a13e4e4049
Tri <trisentosawisesa@gmail.com>
2021-01-21 19:32:55 -0600

    dr github commit

--- public/javascripts/citiesAndCountries.js
+++ public/javascripts/citiesAndCountries.js
@@ -4,12 +4,13 @@ import usDatas from '../data/usCitiesAndStates.js'
 const states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", 'Lousiana', "Maine", "Maryland","Massachusetts","Michigan", "Minnesotta", "Mississippi", "Missouri","Montana", 'Nebraska', "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma","Oregon","Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 const stateID = ["01", "54", "02", "03", "04", "05", "06", "07", "08", "09", "10", "52", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
 const countrySelect = document.getElementById("country");
-let citiesSelect = document.getElementById('city');
-const stateOption = document.getElementById("stateOption");
 const stateSelect = document.getElementById("state");
+const citiesSelect = document.getElementById('city');
+const stateOption = document.getElementById("stateOption");
 let countryIndex = countrySelect.options.selectedIndex;
 let countryName = countrySelect.options.item(countryIndex).text;
 let country = cityDatas[countryName];
+
 
 for (let i = 0; i< states.length; i++) {
     let option = document.createElement("option");
